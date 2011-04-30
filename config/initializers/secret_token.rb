97c09aa31ee82039d176e7f65c0f009614117752
Ivan Loire <ivan@iloire.com>
2011-04-30 12:01:34 +0200

    Initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContratosAragon::Application.config.secret_token = '427e8813b5fef234de2f03b123d0b0e6b9801f44df929255983d5b4e05ade3343136733585e9551d1fbcf7a144bff34ed9b6d607c1e5fa51abbc5ccd764d95e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ContratosAragon::Application.config.session_store :cookie_store, :key => '_ContratosAragon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ContratosAragon::Application.config.session_store :active_record_store
