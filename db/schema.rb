3a90ecb2d83763fab0554e8e1f6dd4dd4037b0e5
Ivan Loire <ivan@iloire.com>
2011-05-02 00:22:39 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110430115426) do
+ActiveRecord::Schema.define(:version => 20110501213720) do
 
   create_table "contracts", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110430115426) do
     t.string   "resolution_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "success"
   end
 
 end
